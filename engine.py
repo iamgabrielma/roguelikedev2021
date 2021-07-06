2ab2f6f92443b21bcc21a76a760b02b57dfc731a
iamgabrielma <gabrielmaldonado.art@gmail.com>
2021-07-06 19:19:59 +0700

    Moved event handler, render, and init to the Engine class

--- engine.py
+++ engine.py
@@ -1,7 +1,14 @@
+from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
+
+from actions import EscapeAction, MovementAction
+from entity import Entity
+from input_handlers import EventHandler 
+
 class Engine:
-"""
-Take the responsibily of drawing the map and entities, as well as handling the player’s input. 
-"""
+
 	def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
 		"""
 		- entities: We'll use a SET, as elements must be unique and can't be added twice.
@@ -12,12 +19,27 @@ Take the responsibily of drawing the map and entities, as well as handling the p
 		self.event_handler = event_handler
 		self.player = player
 
-	def handle_events():
+	def handle_events(self, events: Iterable[Any]) -> None:
 		"""
-		"""
-		pass
 
-	def render():
 		"""
+		for event in events:
+			action = self.event_handler.dispatch(event)
+
+			if action is None:
+				continue
+			if isinstance(action, MovementAction):
+				self.player.move(dx=action.dx, dy=action.dy)
+			elif isinstance(action, EscapeAction):
+				raise SystemExit()
+
+	def render(self, console: Console, context: Context) -> None:
 		"""
-		pass
+		Handles screen drawing
+		"""
+		for entity in self.entities:
+			console.print(entity.x, entity.y, entity.char, fg=entity.color)
+
+		context.present(console)
+		console.clear()
+		
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-
 import tcod
-from actions import EscapeAction, MovementAction
+#from actions import EscapeAction, MovementAction
+
 from input_handlers import EventHandler
 from entity import Entity
+from engine import Engine
 
 def testing_func():
     return "Testing"
@@ -24,10 +25,13 @@ def main() -> None:
     )
 
     # Instanciate our event handler
-    eventHandler = EventHandler()
+    event_handler = EventHandler()
 
-    # Instantiate the player Entity
+    # Instantiate the player Entity and add it to our entities dictionary
     player = Entity(int(screen_width/2), int(screen_height/2), "@", (255, 255, 255))
+    entities = {player}
+
+    engine = Engine(entities= entities , event_handler= event_handler, player= player)
 
     # Creates the screen ( the Terminal )
     with tcod.context.new_terminal(
@@ -42,20 +46,23 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         # Game loop starts:
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
+            #root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
             # present() is what will print on screen, is like the loop Update() method
             context.present(root_console)
-            root_console.clear() # We clear the console before running the next loop
+            #root_console.clear() # We clear the console before running the next loop
             # Graceful program exit without crashing it, by pressing X in the console window.
-            for event in tcod.event.wait(): # this waits for user input to process.
-                action = eventHandler.dispatch(event) # we send the event to our event_handler dispatch method, which will return an action as response
+            # for event in tcod.event.wait(): # this waits for user input to process.
+            #     action = event_handler.dispatch(event) # we send the event to our event_handler dispatch method, which will return an action as response
 
-                if action is None:
-                    continue
-                if isinstance(action, MovementAction): # TODO: Research isinstance()
-                    player.move(dx=action.dx, dy=action.dy)
-                if isinstance(action, EscapeAction):
-                    raise SystemExit()
+            #     if action is None:
+            #         continue
+            #     if isinstance(action, MovementAction): # TODO: Research isinstance()
+            #         player.move(dx=action.dx, dy=action.dy)
+            #     if isinstance(action, EscapeAction):
+            #         raise SystemExit()
 
 
 if __name__ == "__main__":
