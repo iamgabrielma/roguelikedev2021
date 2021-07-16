0ecff0522b0f15216d293f45f9d2242c47c8a5a6
iamgabrielma <gabrielmaldonado.art@gmail.com>
2021-07-16 13:41:52 +0700

    Added tunnel_between func to connect rooms

--- procGen.py
+++ procGen.py
@@ -1,7 +1,10 @@
 # This will house our Procedural Map Generator
-from typing import Tuple
+import tcod
+import random
+from typing import Tuple, Iterator
 from game_map import GameMap
 import tile_types
+from __future__ import annotations
 
 
 class RectangularRoom:
@@ -28,6 +31,15 @@ class RectangularRoom:
         """Returns the inner area of this room as a 2D array index."""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
 
+    def intersects(self, other: RectangularRoom) -> bool:
+        """Returns True if this room overlaps with another RectangularRoom"""
+        return(
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 >= other.y2
+            and self.y2 >= other.y1
+        )
+
 def generate_dungeon(map_width, map_height) -> GameMap:
 
     dungeon = GameMap(map_width, map_height)
@@ -38,4 +50,23 @@ def generate_dungeon(map_width, map_height) -> GameMap:
     dungeon.tiles[room_1.inner] = tile_types.floor
     dungeon.tiles[room_2.inner] = tile_types.floor
 
+    for x, y in tunnel_between(room_2.center, room_1.center):
+        dungeon.tiles[x,y] = tile_types.floor
+
     return dungeon
+
+def tunnel_between(start: Tuple[int,int], end: Tuple[int,int]) -> Iterator[Tuple[int,int]]:
+    """ Takes 2 arguments, both Tuples with an X and Y coordinates, and Returns another Tuple representing an L-shaped tunnel between these 2 points """
+    x1, y1 = start
+    x2, y2 = end
+
+    if random.random() < 0.5: # 50% chance
+        corner_x, corner_y = x2, y1 # Move horizontally, then vertically.
+    else:
+        corner_x, corner_y = x1, y2 # Move vertically, then horizontally.
+
+    # Generate the coordinates for this tunnel.
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y # Returns a generator
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y # Returns a generator
